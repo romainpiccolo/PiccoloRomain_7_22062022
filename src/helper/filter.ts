72170d42625f20d8f26c6914ff61e8ad9803b43f
Romain <piccolo.rom@gmail.com>
2022-06-13 17:20:05 +0200

    filters DONE

--- src/helper/filter.ts
+++ src/helper/filter.ts
@@ -1,4 +1,5 @@
 import IRecipe from "../Interface/IRecipe";
+import IRecipeIngredient from "../Interface/IRecipeIngredient";
 
 const checkInIngredient = (recipe: IRecipe, lowerSearch: string) => {
     return recipe.ingredients.filter(item => {
@@ -18,20 +19,40 @@ const filterRecipesByNameDescriptionAndIngredients = (filterRecipes: IRecipe[], 
     })
 }
 
-const filterRecipesByIngredient = (filterRecipes: IRecipe[], ingredientList: Array<string>) => {
-    console.log(filterRecipes);
+const extractIngredientFromRecipe = (ingredientList: IRecipeIngredient[]) => {
+    const ingredients: string[] = []
+    ingredientList.map(item => {
+        ingredients.push(item.ingredient.toLowerCase())
+    })
 
-    return filterRecipes
+    return ingredients
+}
+
+const filterRecipesByIngredient = (filterRecipes: IRecipe[], ingredientList: Array<string>) => {
+    return filterRecipes.filter(recipe => {
+        const recipeIngredients = extractIngredientFromRecipe(recipe.ingredients)
+
+        return ingredientList.every(ingredient => recipeIngredients.includes(ingredient))
+    })
 }
 
 
 const filterRecipesByAppliance = (filterRecipes: IRecipe[], applianceList: Array<string>) => {
+
+    if (!applianceList.length) {
+        return filterRecipes
+    }
+
     return filterRecipes.filter(recipe => {
-        return applianceList.includes(recipe.appliance.toLowerCase())
+        return applianceList.every(appliance => appliance === recipe.appliance.toLowerCase())
     })
 }
 
 const filterRecipesByUstensil = (filterRecipes: IRecipe[], ustensilList: Array<string>) => {
+
+    if (!ustensilList.length) {
+        return filterRecipes
+    }
 
     return filterRecipes.filter(recipe => {
         return ustensilList.every(ustensil => recipe.ustensils.includes(ustensil))
@@ -41,22 +62,9 @@ const filterRecipesByUstensil = (filterRecipes: IRecipe[], ustensilList: Array<s
 const filterByTags = (filterRecipes: IRecipe[], ingredientList: Array<string>, applianceList: Array<string>, ustensilList: Array<string>) => {
     const resultIngredient: IRecipe[] = filterRecipesByIngredient(filterRecipes, ingredientList)
     const resultAppliance: IRecipe[] = filterRecipesByAppliance(resultIngredient, applianceList)
-
-    console.log('resultAppliance', resultAppliance);
-
-    //TODOOOOOOOOOOOOOOOOOOOOOOOOOOO
     const resultUstensil: IRecipe[] = filterRecipesByUstensil(resultAppliance, ustensilList)
 
-    const t = new Set();
-
-    resultUstensil.map(recipe => {
-        t.add(recipe)
-    })
-
-    // console.log(Array.from(t));
-
-
-    return Array.from(t) as IRecipe[];
+    return Array.from(new Set(resultUstensil)) as IRecipe[];
 }
 
 
